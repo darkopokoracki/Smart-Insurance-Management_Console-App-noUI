89754f3d8abafaaa8574820f5615f2ece441ecc1
Darko <Darko.Pokoracki@adacta-fintech.com>
2025-04-17 21:50:10 +0200

    Policy

---
+++ SmartInsuranceManagementSystem/Policy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInsuranceManagementSystem
+{
+    internal class Policy
+    {
+        private string _product;
+        private string _coverage;
+        private User _policyholder;
+        private User _agent;
+
+        public Policy(string product, string coverage, User policyholder, User agent)
+        {
+            this._product = product;
+            this._coverage = coverage;
+            this._policyholder = policyholder;
+            this._agent = agent;
+        }
+    }
+}
